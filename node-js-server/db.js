5a11d5b9f47f910d34d6e34a7c12cda2d2817207
user <kenseiks123@gmail.com>
2025-06-01 19:25:26 +0900

    DB接続情報の修正漏れ

--- node-js-server/db.js
+++ node-js-server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'boarddb',
-    password: 'postgres',
-    port: 5432,
+  user: 'board_db_2jat_user',
+  host: 'dpg-d0tr1pu3jp1c73etd9bg-a',
+  database: 'board_db_2jat',
+  password: 'NhmaBRj4tteZjcsTojr5pLCxtatIQwNU',
+  port: 5432,
 });
 
 module.exports = pool;
